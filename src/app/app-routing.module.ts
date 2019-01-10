4e61d0dbcd278477780dcbcd86fe6698661c0ae8
Lin-z Chang <lin-zchang@amrock.com>
2019-01-10 10:40:18 -0500

    Added global styling, changed some SCSS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ToDoListComponent },
+  { path: 'to-do-list', component: ToDoListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
